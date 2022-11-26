a12bc430c8df479196469fafb1b49a34c502f18e
rochelryu <core.irie@gmail.com>
2022-11-26 22:52:42 +0000

    Version stable 1.0.16

---
+++ lib/Pages/list_filleul.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ListFilleul extends StatefulWidget {
+  const ListFilleul({Key key}) : super(key: key);
+
+  @override
+  _ListFilleulState createState() => _ListFilleulState();
+}
+
+class _ListFilleulState extends State<ListFilleul> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
