0ae3411c5207d4aa67cab115ba6dce14cc31fc10
rochelryu <core.irie@gmail.com>
2023-02-28 11:56:55 +0000

    prepare 1.0.25

---
+++ lib/Pages/view_picture.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ViewPicture extends StatelessWidget {
+  const ViewPicture({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
